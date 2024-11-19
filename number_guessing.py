44f6bd49f1a1b1d1c119a8705c3e2cfefaba49f8
pbpsandwhich <pbpsandwhich@protonmail.com>
2024-11-19 14:21:02 -0700

    "Corrected an error in the conditional logic. Easy mode now has 10 chances and hard has 5 chances"

--- number_guessing.py
+++ number_guessing.py
@@ -11,6 +11,22 @@ def game_choice(difficulty_level):
     guess_count = 0
     #print(f"My guessed number is {number}")
     if difficulty_level == "easy":
+        while guess_count < 10:
+            users_guess = int(input("Make a guess: "))
+            if users_guess == number:
+                print("That's the right number congrats you win")
+                guess_count = 11
+            elif users_guess < number:
+                print("Your number is lower than mine, guess again: ")
+                guess_count += 1
+                if guess_count == 10:
+                    print("Sorry you loose, too many guesses!")
+            elif users_guess > number:
+                print("Your number is higher than mine, guess again: ")
+                guess_count += 1
+                if guess_count == 10:
+                    print("Sorry you loose, too many guesses!")
+    if difficulty_level == "hard":
         while guess_count < 5:
             users_guess = int(input("Make a guess: "))
             if users_guess == number:
@@ -25,22 +41,6 @@ def game_choice(difficulty_level):
                 print("Your number is higher than mine, guess again: ")
                 guess_count += 1
                 if guess_count == 5:
-                    print("Sorry you loose, too many guesses!")
-    if difficulty_level == "hard":
-        while guess_count < 10:
-            users_guess = int(input("Make a guess: "))
-            if users_guess == number:
-                print("That's the right number congrats you win")
-                guess_count = 6
-            elif users_guess < number:
-                print("Your number is lower than mine, guess again: ")
-                guess_count += 1
-                if guess_count == 10:
-                    print("Sorry you loose, too many guesses!")
-            elif users_guess > number:
-                print("Your number is higher than mine, guess again: ")
-                guess_count += 1
-                if guess_count == 10:
                     print("Sorry you loose, too many guesses!")
 
 
